e589022da4ec3ba3515a140ed31bd225f049604c
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-04-26 12:28:21 +0200

    Added Torchlight to light the path

---
+++ project/src/core/things/TorchLight.cpp
@@ -0,0 +1,37 @@
+#include "TorchLight.h"
+
+void TorchLight::init()
+{
+    this->drawable = dh::create_from_model("objCandle.obj", Transform({0, 0, -5}, {0, 0, 0}, {0.1, 0.1, 0.1}));
+    Material *m = dh::create_material(ShaderProg::SIMPLE, {1, 1, 1}, {0.8, 0.8, 0.8, 16});
+    //dh::attach_texture_to_material(m, 0, "conc");
+    drawable->setMaterial(m);
+}
+
+void TorchLight::update(float dt)
+{
+}
+
+Drawable *TorchLight::get_drawable()
+{
+    return this->drawable;
+}
+
+void TorchLight::cleanup()
+{
+}
+
+void TorchLight::on_notify(MailTopic topic, void* aux)
+{
+    // demo observer pattern
+    switch (topic)
+    {
+    case DA_WAE:
+    {
+        debug_log("Torch Light: LET ME SHOW YOU DA WAE!\n");
+    }
+    break;
+    default:
+        break;
+    }
+}
---
+++ project/src/core/things/TorchLight.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../entity/Thing.h"
+#include "../entity/Drawable.h"
+#include "../../util/Camera.h"
+#include "../../util/Input.h"
+#include "../../util/Debug.h"
+
+#include "../system/DrawableHelper.h"
+#include "../system/ThingMailbox.h"
+
+class TorchLight : public Thing
+{
+public:
+    TorchLight(ThingMailbox* mailbox) : mailbox(mailbox) {}
+
+    void init() override;
+    void update(float dt) override;
+    void cleanup() override;
+    void on_notify(MailTopic topic, void* aux) override;
+
+    Drawable* get_drawable();
+private:
+    Drawable* drawable;
+    ThingMailbox* mailbox;
+
+
+
+};
